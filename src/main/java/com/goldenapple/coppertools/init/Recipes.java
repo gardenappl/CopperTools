82591ecba3b46f049fcac8a4eae4265ab060dc89
goldenapple3 <goldenappleminecraft@gmail.com>
2014-09-04 17:55:22 +0300

    Recipes

--- src/main/java/com/goldenapple/coppertools/init/Recipes.java
+++ src/main/java/com/goldenapple/coppertools/init/Recipes.java
@@ -1,5 +1,6 @@
 package com.goldenapple.coppertools.init;
 
+import com.goldenapple.coppertools.handler.ConfigHandler;
 import cpw.mods.fml.common.registry.GameRegistry;
 import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
@@ -7,7 +8,16 @@ import net.minecraftforge.oredict.ShapedOreRecipe;
 
 public class Recipes {
     public static void init(){
-        GameRegistry.addRecipe(new ShapedOreRecipe(new ItemStack(ModItems.copperPick),"iii", " s ", " s ", 'i', "ingotCopper", 's', "stickWood"));
         GameRegistry.addRecipe(new ShapedOreRecipe(new ItemStack(ModItems.woodSaber)," i ", " i ", " s ", 'i', "logWood", 's', "stickWood"));
+
+        if (ConfigHandler.loadCopper = true) {
+            GameRegistry.addRecipe(new ShapedOreRecipe(new ItemStack(ModItems.copperPick), "iii", " s ", " s ", 'i', "ingotCopper", 's', "stickWood"));
+            GameRegistry.addRecipe(new ShapedOreRecipe(new ItemStack(ModItems.copperSword), " i ", " i ", " s ", 'i', "ingotCopper", 's', "stickWood"));
+            GameRegistry.addRecipe(new ShapedOreRecipe(new ItemStack(ModItems.copperShovel), " i ", " s ", " s ", 'i', "ingotCopper", 's', "stickWood"));
+            GameRegistry.addRecipe(new ShapedOreRecipe(new ItemStack(ModItems.copperAxe), "ii ", "is ", " s ", 'i', "ingotCopper", 's', "stickWood"));
+            GameRegistry.addRecipe(new ShapedOreRecipe(new ItemStack(ModItems.copperAxe), " ii", " si", " s ", 'i', "ingotCopper", 's', "stickWood"));
+            GameRegistry.addRecipe(new ShapedOreRecipe(new ItemStack(ModItems.copperHoe), "ii ", " s ", " s ", 'i', "ingotCopper", 's', "stickWood"));
+            GameRegistry.addRecipe(new ShapedOreRecipe(new ItemStack(ModItems.copperHoe), " ii", " s ", " s ", 'i', "ingotCopper", 's', "stickWood"));
+        }
     }
 }
