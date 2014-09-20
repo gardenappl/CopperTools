a9280b17902664a876c9be125c98ca17955113fb
goldenapple3 <goldenappleminecraft@gmail.com>
2014-09-20 12:56:14 +0300

    MiscUtil is now OreHelper

--- src/main/java/com/goldenapple/coppertools/util/OreHelper.java
+++ src/main/java/com/goldenapple/coppertools/util/OreHelper.java
@@ -3,7 +3,7 @@ package com.goldenapple.coppertools.util;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.oredict.OreDictionary;
 
-public class MiscUtil {
+public class OreHelper {
     public static boolean isItemThisOre(ItemStack item, String ore){
         for (int o : OreDictionary.getOreIDs(item)) {
             if (OreDictionary.getOreName(o).equals(ore)) {
