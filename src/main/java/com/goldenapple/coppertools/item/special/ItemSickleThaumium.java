f4423834ec46f765d16564a561664874b666137c
goldenapple3 <goldenappleminecraft@gmail.com>
2015-02-27 16:54:03 +0200

    Fix the typo!

--- src/main/java/com/goldenapple/coppertools/item/special/ItemSickleThaumium.java
+++ src/main/java/com/goldenapple/coppertools/item/special/ItemSickleThaumium.java
@@ -6,7 +6,7 @@ import cpw.mods.fml.common.Optional;
 import thaumcraft.api.IRepairable;
 import thaumcraft.api.ThaumcraftApi;
 
-@Optional.Interface(iface = "IRepairable", modid = "Thaumcraft")
+@Optional.Interface(iface = "thaumcraft.api.IRepairable", modid = "Thaumcraft")
 public class ItemSickleThaumium extends ItemSickleCommon implements IRepairable{
     public ItemSickleThaumium(){
         super(ThaumcraftApi.toolMatThaumium, Names.thaumiumSickle, "ingotThaumium", false);
