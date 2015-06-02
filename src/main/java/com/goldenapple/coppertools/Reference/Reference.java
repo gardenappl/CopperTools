0ca26843cb1f616745bb8ec13647233f96946e66
goldenapple3 <goldenappleminecraft@gmail.com>
2015-06-02 15:20:54 +0300

    1.4 update!

--- src/main/java/com/goldenapple/coppertools/Reference/Reference.java
+++ src/main/java/com/goldenapple/coppertools/Reference/Reference.java
@@ -3,7 +3,8 @@ package com.goldenapple.coppertools.reference;
 public class Reference {
     public static final String MOD_ID = "CopperTools";
     public static final String MOD_NAME = "Copper Tools";
-    public static final String VERSION = "1.7.10-1.3";
+    public static final String VERSION = "1.7.10-1.4";
 
     public static final String GUI_FACTORY = "com.goldenapple.coppertools.gui.CopperToolsGuiFactory";
+    public static final String DEPENDANCIES = "after:PneumaticCraft";
 }
